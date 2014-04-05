dbc645ca6826c6b47977c55fcd7b6739d89402bd
James Stewart <jys@ketlai.co.uk>
2014-04-05 21:51:23 +0100

    Initial commit of blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalendarAnnotator::Application.config.secret_key_base = '22b42802fac5c62a53a5e0660a31db4cb4c3d0b1d94496912145452b233a41c437197fb8604cc067ac262949253cc9e46554d517c0e0e8590f37cfb76c60134e'
