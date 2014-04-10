e4b47f3ca273654c86d9c434043082ae252642ce
James Stewart <jys@ketlai.co.uk>
2014-04-10 21:47:07 +0100

    Ajax for updating notes

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,12 +1,18 @@
 class AnnotationsController < ApplicationController
   def create
     @annotation = current_user.annotations.create!(params[:annotation].permit(:event_id, :notes))
-    redirect_to :back, notice: "Annotation created"
+    respond_to do |wants|
+      wants.html { redirect_to :back, notice: "Annotation created" }
+      wants.js   { render text: annotation_path(@annotation), status: :created }
+    end
   end
 
   def update
     @annotation = current_user.annotations.find(params[:id])
     @annotation.update_attributes!(params[:annotation].permit(:event_id, :notes))
-    redirect_to :back, notice: "Annotation created"
+    respond_to do |wants|
+      wants.html { redirect_to :back, notice: "Annotation updated" }
+      wants.js   { head :ok }
+    end
   end
 end
