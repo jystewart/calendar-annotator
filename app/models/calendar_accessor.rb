98e7bc70e3b642f9781df4fb33e86c29eb8332da
James Stewart <jys@ketlai.co.uk>
2014-04-10 17:02:00 +0100

    Pull google config out to use the new secrets.yml

--- app/models/calendar_accessor.rb
+++ app/models/calendar_accessor.rb
@@ -2,7 +2,11 @@ require 'oauth2'
 
 class CalendarAccessor
   def initialize(auth_token)
-    client = OAuth2::Client.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], :site => 'https://www.googleapis.com')
+    client = OAuth2::Client.new(
+      Rails.application.secrets.google_client_id,
+      Rails.application.secrets.google_client_secret,
+      site: 'https://www.googleapis.com'
+    )
     @token = OAuth2::AccessToken.new(client, auth_token)
   end
 
