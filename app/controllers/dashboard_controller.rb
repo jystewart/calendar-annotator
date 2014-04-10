6705043df0b8ebc2207fedaf4bf94990e3334ed0
James Stewart <jys@ketlai.co.uk>
2014-04-10 17:01:28 +0100

    Extract an AnnotatedEventList class to manage list building
    It's a bit rough and ready, full of class variables, but it's a start.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,33 @@
 class DashboardController < ApplicationController
   rescue_from OAuth2::Error, with: :reauthenticate
 
+  class AnnotatedEventList
+    def self.build(events, annotations)
+      sorted_event_list(events).collect do |e|
+        {
+          event: e,
+          annotation: annotation_for(e, annotations)
+        }
+      end
+    end
+
+    def self.sorted_event_list(events)
+      events.sort_by do |i|
+        if i['start'].nil?
+          "0"
+        elsif ! i['start']['dateTime'].nil?
+          i['start']['dateTime']
+        else
+          "1"
+        end
+      end
+    end
+
+    def self.annotation_for(event, annotations)
+      annotations.find { |a| a.event_id == event['id'] } || Annotation.new(event_id: event['id'])
+    end
+  end
+
   def index
     @events = calendar_accessor.calendar_list(Date.today)
   end
@@ -10,7 +37,12 @@ class DashboardController < ApplicationController
   end
 
   def day
-    @event_list = calendar_accessor.event_list(params[:id], Date.parse(params[:date]))
+    @day = Date.parse(params[:date])
+    event_list = calendar_accessor.event_list(params[:id], @day)
+    annotations = current_user.annotations.where(
+      event_id: event_list['items'].collect { |i| i['id'] }
+    )
+    @event_list = AnnotatedEventList.build(event_list['items'], annotations)
   end
 
   protected
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,4 @@ module ApplicationHelper
   def display_start_time(item)
     DateTime.parse(item['start']['dateTime']).strftime("%k:%M")
   end
-
-  def sorted_item_list(items)
-    items.sort_by do |i|
-      if i['start'].nil?
-        "0"
-      elsif ! i['start']['dateTime'].nil?
-        i['start']['dateTime']
-      else
-        "1"
-      end
-    end
-  end
 end
