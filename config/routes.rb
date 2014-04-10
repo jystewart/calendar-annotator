ed47e2e92c070d993e02867550398522f014247a
James Stewart <jys@ketlai.co.uk>
2014-04-10 15:40:05 +0100

    Allow underscores in calendar urls

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ CalendarAnnotator::Application.routes.draw do
     get "/signout" => "devise/sessions#destroy"
   end
 
-  constraints(:id => /[0-9A-Za-z\-\.@]+/) do
+  constraints(:id => /[0-9A-Za-z\-\.@_]+/) do
     get "/calendars/:id" => "dashboard#calendar", as: :calendar
     get "/calendars/:id/:date" => "dashboard#day", as: :day, constraints: { date: /\d{4}-\d{2}-\d{2}/ }
   end
